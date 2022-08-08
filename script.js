430284921a5ce78e2606589100e819394e9f77c8
John <jyakou@gmx.fr>
2022-08-08 09:38:54 +0200

    08/08/2022 - 09h38

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ var checkDead = setInterval(function(){
     if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
-        alert("Tu a perdu");
+        alert("Tu as perdu");
     }
 }, 10);
